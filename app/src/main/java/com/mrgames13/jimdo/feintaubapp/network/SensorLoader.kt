a81f8209eed70350f354347e6b44d1970e60516d
Marc Auberer <marc.auberer@chillibits.com>
2020-04-03 02:14:59 +0200

    Next dev version

---
+++ app/src/main/java/com/mrgames13/jimdo/feintaubapp/network/SensorLoader.kt
@@ -0,0 +1,9 @@
+/*
+ * Copyright © Marc Auberer 2017 - 2020. All rights reserved
+ */
+
+package com.mrgames13.jimdo.feintaubapp.network
+
+suspend fun loadSensors() {
+
+}
--- app/src/main/java/com/mrgames13/jimdo/feintaubapp/ui/fragment/RankingFragment.kt
+++ app/src/main/java/com/mrgames13/jimdo/feintaubapp/ui/fragment/RankingFragment.kt
@@ -7,40 +7,55 @@ package com.mrgames13.jimdo.feintaubapp.ui.fragment
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
-import android.view.View.GONE
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.mikepenz.fastadapter.FastAdapter
+import com.mikepenz.fastadapter.adapters.ItemAdapter
 import com.mrgames13.jimdo.feintaubapp.R
 import com.mrgames13.jimdo.feintaubapp.network.RANKING_CITY
 import com.mrgames13.jimdo.feintaubapp.network.RANKING_COUNTRY
 import com.mrgames13.jimdo.feintaubapp.network.loadRanking
+import com.mrgames13.jimdo.feintaubapp.ui.item.RankingItem
 import kotlinx.android.synthetic.main.fragment_ranking.view.*
-import kotlinx.android.synthetic.main.item_ranking.view.*
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 class RankingFragment(private val mode: Int) : Fragment() {
+
+    // Variables as objects
+    private lateinit var adapter: ItemAdapter<RankingItem>
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return inflater.inflate(R.layout.fragment_ranking, container, false).run {
+            // Initialize recycler view
+            rankingList.layoutManager = LinearLayoutManager(context)
+            rankingList.setHasFixedSize(true)
+            adapter = ItemAdapter()
+            rankingList.adapter = FastAdapter.with(adapter)
+
+            // Load ranking
             loadData(this)
             this
         }
     }
 
-    private fun loadData(rv: View) {
+    private fun loadData(view: View) {
         CoroutineScope(Dispatchers.IO).launch {
             val mode = if(mode == 0) RANKING_CITY else RANKING_COUNTRY
-            val ranking = loadRanking(requireContext(), mode)
-            CoroutineScope(Dispatchers.Main).launch {
-                ranking.forEachIndexed {index, item ->
-                    val rank = LayoutInflater.from(requireContext()).inflate(R.layout.item_ranking, null, false)
-                    rank.itemRank.text = "${index +1}"
-                    rank.itemCountryCity.text = if(mode == RANKING_CITY) item.country + ", " + item.city else item.country
-                    rank.itemCount.text = item.count.toString()
-                    rv.rankingContainer.addView(rank)
-                }
-                rv.rankingLoading.visibility = GONE
+            val rankingItems = ArrayList<RankingItem>()
+            loadRanking(requireContext(), mode).forEachIndexed { index, item ->
+                val rankingItem = RankingItem()
+                rankingItem.rank = index +1
+                rankingItem.countryCity = if(mode == RANKING_CITY) item.country + ", " + item.city else item.country
+                rankingItem.sensorCount = item.count
+                rankingItems.add(rankingItem)
+            }
+            withContext(Dispatchers.Main) {
+                adapter.add(rankingItems)
+                view.rankingLoading.visibility = View.GONE
             }
         }
     }
